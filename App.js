b1030d879cfa330e02517a840328affe12c93be2
seba13 <rodrigo.tolaba@davinci.edu.ar>
2021-12-05 04:55:30 -0300

    TP-3 AuthSession

--- App.js
+++ App.js
@@ -1,17 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+import Tab2 from './components/Tab2';
+import Home from './components/Home';
+import Login2 from './components/login';
+import Personajes from './components/Personajes';
+import { Icon } from 'react-native-elements';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+class stackN extends React.Component{
+  render(){
+    return (
+      <Stack.Navigator screenOptions={ {headerStyle:{backgroundColor: '#f3627f'}} }>
+          <Stack.Screen name="Home" component={Home}/>
+          <Stack.Screen name="Personajes" component={Personajes} options={ {title: 'Personajes',
+              headerStyle:{backgroundColor: '#f3627f'}} }/>
+        </Stack.Navigator>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+class navegation extends React.Component{
+  render(){
+    return(
+      
+        <Tab.Navigator screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'stackN') {
+            iconName = focused ? 'home' : 'home';
+          } else if (route.name === 'Tab2') {
+            iconName = focused ? 'email' : 'email';
+          }
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+        headerShown:false,
+      })} >
+        <Tab.Screen name="stackN" component={stackN}/>
+        <Tab.Screen name="Tab2" component={Tab2}/>
+      </Tab.Navigator>
+
+      
+    );
+  }
+}
+
+export default class Login extends React.Component{
+  render(){
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={ {headerStyle:{backgroundColor: '#f3627f'}} }>
+            <Stack.Screen name="Login" component={Login2}/>
+            <Stack.Screen name="navegation" component={navegation} options={ {headerShown:false,title: 'Inicio',
+                headerStyle:{backgroundColor: '#f3627f'}} }/>
+          </Stack.Navigator>
+        </NavigationContainer>
+    );
+  }
+}
+
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
